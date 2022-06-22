0261c4922da4c9243bf593838da8f95057e655df
Sanskar Modi <101720291+sansProg@users.noreply.github.com>
2022-06-22 21:55:54 +0530

    Update and rename script1.js to main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 // returns a random numbr from 1-6
 function diceRoll() {
   return Math.floor(Math.random() * 6) + 1;
-  // Math.random returns a random num from interval [0,1). so we multiply it by 6 to get number from interval [0,6) and we round off that number using Math.floor that the number generated will be a whole number belonging to interval [0,5] and we added 1 to get a whole number from interval [1,6]
 }
 
 // update image based on the dice roll 
@@ -10,12 +9,10 @@ function updateImg(classs, n) {
   document
     .querySelector(classs) 
     .setAttribute("src", "images/dice" + n + ".png");
-    // here, query selector selects the first selector having the class as classs  and setattribute sets the attribute passed in as 1st parameter to the second parameter , ie sets src to the specified position as 2nd parameter
 }
 
 function displayResult(result) {
   document.querySelector("h1").innerHTML = result;
-  // innerHTML gives all the content inside an Object, here all the content in the h1 tag
 }
 
 var roll1 = diceRoll();
